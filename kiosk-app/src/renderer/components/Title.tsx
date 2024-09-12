f18ff5af5b9bd921c528c2824ab080cac462373b
오현진 <ohj220@naver.com>
2024-09-12 13:13:29 +0900

    [#3] 상단제목, 하단버튼 컴포넌트 추가 및 시작페이지 다음을 위한 설정

---
+++ kiosk-app/src/renderer/components/Title.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Title: React.FC = () => {
+  return (
+    <div className="w-full px-[2vw] py-[2vh] flex justify-center items-center">
+      <h1 className="text-center text-[11vw] font-normal font-kaushan tracking-wide">
+        <span className="text-[#f9b812]">Ca</span>
+        <span className="text-black">rtoon </span>
+        <span className="text-[#f9b812]">Ti</span>
+        <span className="text-black">me</span>
+      </h1>
+    </div>
+  );
+};
+
+export default Title;
