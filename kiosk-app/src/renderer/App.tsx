9c2fbabe1f16f422db436003385060b2df7045a2
오현진 <ohj220@naver.com>
2024-10-04 14:19:24 +0900

    [#23] 필요없는 페이지 삭제

--- kiosk-app/src/renderer/App.tsx
+++ kiosk-app/src/renderer/App.tsx
@@ -6,7 +6,6 @@ import { getAuth, signInAnonymously } from "firebase/auth";
 import Layout from "./components/Layout";
 import Start from "./pages/Start";
 import UserVerification from "./pages/UserVerification";
-import RecommendationLoading from "./pages/RecommendationLoading";
 import CartoonRecommendation from "./pages/CartoonRecommendation";
 import EnterSuccess from "./pages/EnterSuccess";
 import ExitSuccess from "./pages/ExitSuccess";
@@ -60,10 +59,6 @@ const App = () => {
             <Route
               path="/user-verification"
               element={<UserVerification sendMessage={sendMessage} />}
-            />
-            <Route
-              path="/recommendation-loading"
-              element={<RecommendationLoading />}
             />
             <Route
               path="/cartoon-recommendation"
